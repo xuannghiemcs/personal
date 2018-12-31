36a8e30d8e99fed6043e877c8aaa3395c9c7deb0
xuannghiemcs <xuanha.nghiem.cs@gmail.com>
2018-12-30 19:10:20 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './awards-boxed.png';
 import './App.css';
 
 class App extends Component {
@@ -13,10 +13,12 @@ class App extends Component {
     // save the users in the state
     this.state = {
       searchOnOff: 0,
+      searchValue: '',
+      countErr: 0,
     };
 
   this.handleClicks = this.handleClicks.bind(this);
-
+  this.handleChange = this.handleChange.bind(this);
 
   }
 
@@ -31,23 +33,50 @@ if(this.state.searchOnOff === 0){
 
   }
 
+  handleChange(e){
+  this.setState({searchValue: e.target.value});
+
+  }
+
   render() {
 
     var searchbar;
 
 if(this.state.searchOnOff === 1){
-searchbar = (      <input type="text" class="searchTerm" placeholder="password"/>);
+searchbar = (      <input type="text" class="searchTerm" height="48" placeholder="password" onChange = {this.handleChange.bind(this)}/>);
+
+}
+
+
+var errMessage = '';
+
+if(this.state.searchValue === "test"){
+
+
+} else if (this.state.searchValue !== ""){
+this.state.countErr += 1;
+  errMessage = "Invalid";
+
+}
+var support = '';
+
+if(this.state.countErr >= 50){
+support = (      <img src={logo} alt="cherry"
+                        width="120" height="120"/>);
 
 }
 
     return (
       <div className="App">
         <header className="App-header">
+      {support}
           <p onClick = {this.handleClicks}>
           Portfolio
           </p>
                 {searchbar}
+    {errMessage}
         </header>
+
       </div>
     );
   }
