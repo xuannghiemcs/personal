eab52a8e825a8bd26e61b7b00171f51dcec3f9bd
xuannghiemcs <xuanha.nghiem.cs@gmail.com>
2019-05-14 23:33:21 -0400

    corrected text

--- src/makeSense.js
+++ src/makeSense.js
@@ -1,5 +1,5 @@
 export default function makeSense(msg) {
 
-return("Hi there, Looks like you haven't been given access.mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
+return("Hi there, Looks like you haven't been given access.");
 
 }
